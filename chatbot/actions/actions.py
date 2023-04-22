7bf38b825ceaa11deb7dd770de6c287925f6ce10
Prithviraj Khelkar <pkhelkar26@gmail.com>
2023-04-22 15:19:39 -0400

    dialogpt

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -4,12 +4,14 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 import mysql.connector
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 connection = mysql.connector.connect(
-    host="",
-    user="",
-    password="",
-    database=""
+    host="localhost",
+    user="root",
+    password="pass@123",
+    database="student"
 )
 
 class OrderStudentCard(Action):
@@ -92,3 +94,40 @@ class GetStudentId(Action):
             dispatcher.utter_template("utter_student_not_found", tracker)
         cursor.close()
         return [SlotSet("student_id", id)]
+
+
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+class ActionGenerateConversation(Action):
+    def name(self):
+        return "action_generate_conversation"
+
+    def run(self, dispatcher, tracker, domain):
+        # Get the user's message from the tracker
+        user_message = tracker.latest_message['text']
+
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(user_message + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if tracker.get_slot('chat_history_ids') else new_user_input_ids
+
+        # Set the hyperparameters
+        max_length = 1000
+        temperature = 0.5
+        top_k = 100
+        top_p = 0.7
+
+        # generated a response while limiting the total chat history to 1000 tokens
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id, temperature=temperature, top_k=top_k, top_p=top_p)
+
+        # Decode the response and send it as a RASA message
+        response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+        dispatcher.utter_message(response)
+
+        # Update the chat history in the tracker
+        tracker.slots['chat_history_ids'] = chat_history_ids
+
+        return []
